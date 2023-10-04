f2d4c3d67f1b35c7dcab89d7598d9e555d074bd8
Kssion <nsurl.cc@gmail.com>
2023-10-04 23:25:48 +0800

    fix port

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=80, log_level="info")
+    uvicorn.run("main:app", port=3000, log_level="info")
 
