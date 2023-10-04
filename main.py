4eb4d5deec8d19f3428a48973cb647459871d7bc
Kssion <nsurl.cc@gmail.com>
2023-10-04 22:42:48 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -13,3 +14,8 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=80, log_level="info")
+
