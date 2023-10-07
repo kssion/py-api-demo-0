9295ca6e0c5f7359a3393f2bb22e7b7768deb470
Kssion <nsurl.cc@gmail.com>
2023-10-07 15:53:08 +0800

    fix vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from typing import Union
 
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
@@ -14,8 +13,4 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=3000, log_level="info")
 
