22033635e4ae35da4e894c327d5130a40c136d4f
Kssion <nsurl.cc@gmail.com>
2023-10-07 16:29:21 +0800

    fix

--- api/index.py
+++ api/index.py
@@ -14,3 +14,13 @@ async def read_root():
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
+@app.get("/api")
+async def read_root():
+    return {"Hello": "api"}
+
+
+@app.get("/{path}")
+async def read_root(path: str):
+    return {"Hello": "World", 'path': path}
+
